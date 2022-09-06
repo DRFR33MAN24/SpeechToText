07681313b813ba83c01ba96b8ba15f1df7fb277c
Gordon Freeman <drfr33man@protonmail.com>
2022-09-06 23:43:35 +0300

    fix sort

--- public/main.js
+++ public/main.js
@@ -250,7 +250,7 @@ const splitAwaited = (path) => {
         minClipLength: clipLength,
         maxClipLength: clipLength,
         outputPath: "tmp/",
-        isStopped: isStoppedObj,
+        isStoppedObj: isStoppedObj,
       },
 
       (err, data) => {
